9d435af01de103db18a44cbda002c9b35cab3e76
mvpetrusenko <mvpetrusenko300@gmail.com>
2024-01-14 17:37:06 +0100

    basic API test

--- API/test_get_artist.py
+++ API/test_get_artist.py
@@ -1,22 +1,4 @@
-"""
-
-
-from api import check_artist_id 
-
-
-def test_check_artist_id():
-    artist_id = '/v1/artists/2CIMQHirSU0MQqyYHq0eOx'
-    check_artist_id(artist_id=artist_id, expected_id="2CIMQHirSU0MQqyYHq0eOx") 
-
-
-
-
-"""    
-
-
 from api import check_artist_id  
-
-#from main import check_artist_id, get_spotify_token
 import requests
 import json
 
@@ -26,8 +8,6 @@ def test_check_artist_id():
     artist_id = '/v1/artists/2CIMQHirSU0MQqyYHq0eOx'
     result = check_artist_id(artist_id=artist_id, expected_id="2CIMQHirSU0MQqyYHq0eOx")
 
-    # Print the result
     print(result)
 
-# Call the test function
 test_check_artist_id()
